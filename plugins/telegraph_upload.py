897238e9d8332ae43a5d1bdb16f707bfb6d56066
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-06 10:29:35 +0530

    Update telegraph_upload.py

--- plugins/telegraph_upload.py
+++ plugins/telegraph_upload.py
@@ -36,7 +36,7 @@ async def getmedia(bot, update):
             Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
     media = update.document or update.video or update.video_note
     medianame = "FayasNoushad/FnTelegraphBot"
-    if(update.media.file_size < 5242880):
+    if(media.file_size < 5242880):
         dwn = await bot.send_message(chat_id=update.chat.id, text=Translation.DOWNLOAD_TEXT, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id)
         await bot.download_media(message=media, file_name=medianame)
         await dwn.edit_text(text=Translation.UPLOADING_TEXT)
