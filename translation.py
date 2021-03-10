ebe4bfe6221407ceb7c29922c855b429fcfd3276
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-10 09:04:40 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,19 +1,17 @@
-from info import Info
-
 class Translation(object):
     START_TEXT = """
-Hello {}, I' am small media or file to `telegra.ph` link uploader bot.
+Hello {}, I' am small media or file to telegra.ph link uploader bot.
 
-Made with love ❤️ in Kerala, India 🇮🇳 by @FayasNoushad. Contact @FayasChat for discussion. Use /help for more...
+Made by @FayasNoushad
 """
     HELP_USER = """
 <b><u>Help and Informations</u></b>
 
 - Just give me a media under 5MB.
 - Then I will download it.
-- I will then upload it to the `Telegra.ph` link.
+- I will then upload it to the telegra.ph link.
 
-Made with love ❤️ in Kerala, India 🇮🇳 by @FayasNoushad. Contact @FayasChat for discussion.
+Made by @FayasNoushad
 """
     DOWNLOAD_TEXT = "<code>Downloading to My Server ...</code>"
     UPLOADING_TEXT = "<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>"
