0eec3efe5c9d48c776403a7d587f8ab57fc3b10b
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-10 09:13:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,9 +18,9 @@ async def button(bot, update):
         await update.message.delete()
 
 @Client.on_message(filters.command(["start"]))
-async def start(client, message):
+async def start(bot, update):
     await bot.send_message(chat_id=update.chat.id, text=Translation.START_TEXT.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Button.START_BUTTONS), reply_to_message_id=update.message_id)
 
 @Client.on_message(filters.command(["help"]))
-async def help(client, message):
+async def help(bot, update):
     await bot.send_message(chat_id=update.chat.id, text=Translation.HELP_USER, parse_mode="html", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Button.HELP_BUTTONS), reply_to_message_id=update.message_id)
