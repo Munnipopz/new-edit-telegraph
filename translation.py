6fe939e7d57980a5847076351977f335ee8cbd1f
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-03-18 20:14:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,11 +6,11 @@ Hello {}, I' am small media or file to telegra.ph link uploader bot.
 - Then I will download it
 - I will then upload it to the telegra.ph link
 
-Made by @FayasNoushad
+Made by @cybersecurityhacker
 """
     DOWNLOAD_TEXT = "<code>Downloading to My Server ...</code>"
     UPLOADING_TEXT = "<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>"
-    SOMETHING_WRONG = "Something Wrong! Contact @FayasChat."
+    SOMETHING_WRONG = "Something Wrong! Contact @cybersecurityhacker."
     FREE_USER_LIMIT_Q_SZE = "Sorry Friend, Free users can only 1 request per {} minutes. Please try again after {} seconds later."
     FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
     BANNED_USER_TEXT = "<code>You are Banned!</code>"
