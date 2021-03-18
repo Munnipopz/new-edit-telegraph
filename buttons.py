d1392132492ae19e741d2bc955e98d3078398f5f
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-03-18 20:04:49 +0530

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,5 +2,5 @@ from pyrogram.types import InlineKeyboardButton
 
 class Button(object):
 
-    START_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')],
-                                      [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat')]]
+    START_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/cybersecurityhacker'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/cybersecurityhacker')]]
+                                      
